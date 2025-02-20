c29d8aa53d95c3a3121e1674a01275aa3438bcd9
p2306662 <p2306662@etu.univ-lyon1.fr>
2025-02-20 19:57:06 +0100

    Ajouté la méthode repr sur Client pour pouvoir débugger son contenu, et fix un problème d'ordre de déclaration

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,7 @@
 class Client :
+
 	__slots__ = "id", "pos", "demande", "intervalle_livraison", "temps_livraison"
+
 	def __init__(self, id: int, pos: tuple[int, int], intervalle_livraison: tuple[int, int], temps_livraison: int = 0, demande: int = 0):
 		self.id = id
 		self.pos = pos
@@ -8,8 +10,34 @@ class Client :
 		self.temps_livraison = temps_livraison
 
 
+	def __repr__(self) -> str :
+		x, y = self.pos
+		début, fin = self.intervalle_livraison
+		temps = self.temps_livraison
+		return f"Client(id: {self.id}, position: ({x} {y}), livraison entre {début} et {fin}, demande: {self.demande}, temps de livraison: {temps})"
+
+
+
+
+
+class Trajet :
+
+	__slots__ = "longueur", "nb_clients", "clients"
+
+	def __init__(self):
+		self.longueur = 0
+		self.nb_clients = 0
+		self.clients = []
+
+
+
+
+
+
 class Flotte :
+
 	__slots__ = "capacite", "nb_camions", "camions", "trajets"
+
 	def __init__(self, capacite: int):
 		self.capacite = capacite
 		self.nb_camions = 0
@@ -23,12 +51,3 @@ class Flotte :
 			self.nb_camions += 1
 		else:
 			raise ValueError ("Quantité supérieure à la capacité maximale !")
-
-
-class Trajet :
-	__slots__ = "longueur", "nb_clients", "clients"
-	def __init__(self):
-		self.longueur = 0
-		self.nb_clients = 0
-		self.clients = []
-
